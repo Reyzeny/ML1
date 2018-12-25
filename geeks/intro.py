cb5816bfef8cea71a87475359f15de10170903ae
pelumzeny <pelumzeny10@gmail.com>
2018-12-25 11:50:57 +0100

    commit

---
+++ geeks/intro.py
@@ -0,0 +1,8 @@
+from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+iris_dataset = load_iris()
+
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset["data"], iris_dataset["target"], random_state=0)
