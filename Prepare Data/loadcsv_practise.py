1a7aa6afcf012f8000717b2a4da77207a4d65666
pelumzeny <pelumzeny10@gmail.com>
2019-01-24 09:11:07 +0100

    WIP

--- Prepare Data/loadcsv_practise.py
+++ Prepare Data/loadcsv_practise.py
@@ -6,6 +6,17 @@ def load_dataset(filename):
     data = list(lines)
     return data
 
+def load_dataset2(filename):
+    dataset = []
+    with open(filename, "r") as file:
+        lines = reader(file)
+        for index, row in enumerate(lines):
+            if not row:
+                continue
+            dataset.append(row)
+            print(index, row)
+    return dataset
+
 
 def str_column_to_float(dataset, column):
     for row in dataset:
@@ -25,10 +36,11 @@ def str_column_to_int(dataset, column):
 
 
 filename = 'iris.csv'
-dataset = load_dataset(filename)
-str_column_to_float(dataset, 0)
-str_column_to_float(dataset, 1)
-str_column_to_float(dataset, 2)
-str_column_to_float(dataset, 3)
-print(str_column_to_int(dataset, 4))
-print(dataset)
+#dataset = load_dataset(filename)
+print(load_dataset2(filename))
+# str_column_to_float(dataset, 0)
+# str_column_to_float(dataset, 1)
+# str_column_to_float(dataset, 2)
+# str_column_to_float(dataset, 3)
+# print(str_column_to_int(dataset, 4))
+# print(dataset)
