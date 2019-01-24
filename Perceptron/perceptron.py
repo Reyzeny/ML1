c15165f467a477430c951981cf03e27fb7a83a8f
pelumzeny <pelumzeny10@gmail.com>
2019-01-24 09:19:07 +0100

    WIP

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -1,2 +1,10 @@
+import reader from csv
+
 def load_csv(filename):
-    
+    dataset = []
+    with open filename as file:
+        lines = reader(file)
+        for row in lines:
+            if row:
+                dataset.append(row)
+        return dataset
